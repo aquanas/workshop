81b23162c314ad97e29ae0a54be24fc6e44e84a0
aquanas <aquanas76@naver.com>
2017-06-11 11:08:31 +0900

    Add files via upload

---
+++ Circuit_21.ino
@@ -0,0 +1,46 @@
+#include <CurieBLE.h>
+
+BLEPeripheral blePeripheral;
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214");
+
+BLEUnsignedCharCharacteristic switchCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214",BLERead | BLEWrite);     
+
+const int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+
+  blePeripheral.setLocalName("101 Board");
+  blePeripheral.setAdvertisedServiceUuid(ledService.uuid());
+
+  blePeripheral.addAttribute(ledService);
+  blePeripheral.addAttribute(switchCharacteristic);
+
+  switchCharacteristic.setValue(0);
+
+  blePeripheral.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  BLECentral central = blePeripheral.central();
+
+  if (central) {
+
+    while(central.connected()) {
+
+      if (switchCharacteristic.written()) {
+        if (switchCharacteristic.value()) {
+          digitalWrite(ledPin, HIGH);
+        }
+        else {
+          digitalWrite(ledPin, LOW);
+        
+          }
+        }
+      }
+    }
+  }
+
